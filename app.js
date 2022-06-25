a98dbeca28ab6d16deb881dfa1244c09c94bc43d
SteamCode <steamcodeyt@gmail.com>
2022-06-25 18:07:19 -0500

    jj

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
     let coins = 0;
-    document.cookie = "coins=1000";
+    //if(document.cookie){
+        coins = 100//document.cookie;
+    //}
+    document.cookie = "username=John Doe; expires=Thu, 18 Dec 2023 12:00:00 UTC; path=/";
+    //document.cookie = "username=1000";
+    document
     $("#collect-coin-bttn").click(function(){
         coins++;
         let action = $("#action");
         action.html("You have " + coins + " coins.");
+        //document.cookie = "username=" + coins;
+        //alert(document.cookie);
     });
+
+
 });
